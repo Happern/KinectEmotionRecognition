f5ec02d9abbbd257f0f60f351795e5f1cf1ec1d1
Happern <happern@Happerns-Mac-mini.local>
2017-03-24 14:41:37 +0300

    trying to remove files from only the repo but not locally
    also added OniPlayer files (still under progress, not yet working)

---
+++ src/ofAppPlayOni.cpp
@@ -0,0 +1,9 @@
+//
+//  ofAppPlayOni.cpp
+//  KinectEmotionRecognition
+//
+//  Created by Happern on 20/03/2017.
+//
+//
+
+#include "ofAppPlayOni.hpp"
---
+++ src/ofAppPlayOni.hpp
@@ -0,0 +1,14 @@
+//
+//  ofAppPlayOni.hpp
+//  KinectEmotionRecognition
+//
+//  Created by Happern on 20/03/2017.
+//
+//
+
+#ifndef ofAppPlayOni_hpp
+#define ofAppPlayOni_hpp
+
+#include <stdio.h>
+
+#endif /* ofAppPlayOni_hpp */
